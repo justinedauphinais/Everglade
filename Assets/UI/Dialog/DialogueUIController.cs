829c7f00d4d64403c8259dbb1d0edbe03378f5d8
justinedauphinais <95946160+justinedauphinais@users.noreply.github.com>
2023-06-20 14:33:12 -0400

    DialogueBox

---
+++ Assets/UI/Dialog/DialogueUIController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class DialogueUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+
+    private TypewriterEffect typewriterEffect;
+
+    private void Start()
+    {
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        CloseDialogueBox();
+        ShowDialogue(testDialogue);
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        yield return new WaitForSeconds(2);
+
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return typewriterEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
+    }
+}
